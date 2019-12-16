c167bfe4a5db236a1dd4507c2bbca70079c33950
warbonnet333 <warbonnet333@gmail.com>
2019-12-17 01:05:05 +0200

    added module 11 promisification

---
+++ module_11/promisification/task_2/script.js
@@ -0,0 +1,30 @@
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false }
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    resolve(updatedUsers);
+  });
+};
+
+const logger = array => console.table(array);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Poly").then(logger);
